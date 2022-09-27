52d3a605806d58a8ac7c3ecf2d53b557d8b42beb
j. van wijnendaele <jovanni.vanwijnendaele@gmail.com>
2022-09-27 14:46:28 +0200

    test

--- richpath/src/main/java/com/richpath/pathparser/PathParser.kt
+++ richpath/src/main/java/com/richpath/pathparser/PathParser.kt
@@ -10,7 +10,9 @@ object PathParser {
      * @return the generated Path object.
      */
     fun createPathFromPathData(pathData: String?): Path {
+        System.out.print("test");
         return PathParser.createPathFromPathData(pathData) ?: Path()
     }
 
 }
+
